04fd6306feafdd3cf1fcd8d35ee6922bf97000af
Uman <213151823+Uman-66@users.noreply.github.com>
2025-07-22 12:42:47 +0500

    Added the basic blueprint GUI of the App

---
+++ modules/elec_calc.py
@@ -0,0 +1,7 @@
+from tkinter import *
+class ElectricityCalc:
+
+    def __init__(self):
+        self.elec =Toplevel()
+        self.elec.title("Electricity Calculator")
+        self.elec.config(padx=20, pady=20)
---
+++ modules/inflation_calc.py
@@ -0,0 +1,7 @@
+from tkinter import *
+class Inflate:
+
+    def __init__(self, ):
+        self.inflt= Toplevel()
+        self.inflt.title("Inflation Observer")
+        self.inflt.config(padx=20, pady=20)
